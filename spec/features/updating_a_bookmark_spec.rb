22d7afbdd95a01fdf0d8e9f26930bd7f77bc61d8
Oliver Cutting <oliver.cutting@gmail.com>
2022-02-10 15:53:05 +0000

    Step14 Complete

---
+++ spec/features/updating_a_bookmark_spec.rb
@@ -0,0 +1,18 @@
+feature 'Updating bookmarks' do
+  scenario 'A user can update a bookmark' do
+    bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+
+    first('.bookmark').click_button('Edit')
+    expect(current_path).to eq("/bookmarks/#{bookmark.id}/edit")
+
+    fill_in('url', with: 'http://snakersacademy.com')
+    fill_in('title', with: 'Snakers Academy')
+    click_button('Submit')
+
+    expect(current_path).to eq('/bookmarks')
+    expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Snakers Academy', href: 'http://snakersacademy.com')
+  end
+end
